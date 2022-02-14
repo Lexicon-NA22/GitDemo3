291a793aa4d7830b07312d60c3817b9466879953
David Nokto <david.nokto@gmail.com>
2022-02-14 11:06:00 +0100

    another prop Added class with property

---
+++ GitDemo3/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo3
+{
+    internal class Class1
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+    }
+}
