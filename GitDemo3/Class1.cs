2ca4a482f17e871e6a36245fb7abad9d2fd6acf3
David Nokto <david.nokto@gmail.com>
2022-02-14 11:16:44 +0100

    Resolved stash conflict

---
+++ GitDemo3/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo3
+{
+    internal class Class1
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+        public int StashedProp { get; set; }
+    }
+}
